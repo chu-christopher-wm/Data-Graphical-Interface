1c9b0bd46250aa3046483a0dbe6a779b27b1cb44
Start Coding Student 2 <session2@START-Coding-MBP-C02PP72CG8WN.local>
2015-10-07 18:08:40 -0700

    -Finished work on graph, CSS, linking, and implementation of HTML.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,35 @@
 /**
  * Created by session2 on 10/7/15.
  */
+google.load('visualization', '1', {
+    packages: ['corechart', 'bar']
+});
+google.setOnLoadCallback(drawMultSeries);
+
+function drawMultSeries() {
+    var data = google.visualization.arrayToDataTable([
+        ['Deaths', '2010 Males', '2010 Females'],
+        ['Malignant Neoplasms', 1540, 3438],
+        ['Peptic Ulcer', 13, 15],
+        ['Diabetes Mellitus', 245, 219],
+        ['Septicemia', 36, 107],
+        ['Atherosclerosis', 46, 74]
+    ]);
+
+    var options = {
+        title: 'Cause of Deaths in 2010',
+        chartArea: {
+            width: '30%'
+        },
+        hAxis: {
+            title: 'Amount of Deaths',
+            minValue: 0
+        },
+        vAxis: {
+            title: 'Cause of Death'
+        }
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
